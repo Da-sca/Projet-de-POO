8380f3d0eea1baabb08b7953b168ec22e0e0d8fd
Da-sca <mbodja8@gmail.com>
2026-07-09 22:07:16 +0000

    Added gitignore

--- Principale.java
+++ Principale.java
@@ -1,17 +1,17 @@
-public class Principale(){
+public class Principale{
 
     public static void main(String[] arg){
 
         // initialisation de la chaine
-        String chaine = "2+3;"; 
+        String chaine = "2+3;";
 
         // creation de l'objet source et initialisation
-        Source source = new Source(chaine);  
+        Source source = new Source(chaine);
 
-        // creation de l'objet analyse et initialisation 
-        Analyseur analyse = new Analyseur(source); 
+        // creation de l'objet analyse et initialisation
+        Analyseur analyse = new Analyseur(source);
 
         // lancement de l'analyseur
-        analyse.analyseur(); 
+        analyse.analyseur();
     }
 }
