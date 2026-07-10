39e4188a76393003e976931f3d75573e346f8862
Ndeye Awa Sene <senendeyeawa60@gmail.com>
2026-07-10 02:45:37 +0000

    Test V2 #3: analyse de '2+3' sans point-virgule - StringIndexOutOfBoundsException

--- Principale.java
+++ Principale.java
@@ -3,7 +3,7 @@ public class Principale{
     public static void main(String[] arg){
 
         // initialisation de la chaine
-        String chaine = "2+3&4;"; //TEST2
+        String chaine = "2+3"; //TEST3
 
         /*TEST 2: On teste l'analyseur avec "2+3&4;" et 
         ça revoie analyse correcte comme le prévoit le CDC.
@@ -11,6 +11,10 @@ public class Principale{
         comme terminée et valide, sans jamais verifier que le reste de la chaine ("$4;") a bien ete consommé.                                            
         Comme analyseur() ne verifie pas non plus la presence reelle du point-virgule final, l'erreur passe                                              
         inapercue.*/
+
+
+        /*TEST 3: Expression sans point virgule; 
+        provoque une exception comme prévu: StringIndexOutOfBoundsException */
 
         // creation de l'objet source et initialisation
         Source source = new Source(chaine);
